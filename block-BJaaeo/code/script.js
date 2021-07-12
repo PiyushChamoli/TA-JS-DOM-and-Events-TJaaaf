fbe32340a3a85e396f6afd6feaf2f25e085a2748
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-07-12 22:09:02 +0530

    update

---
+++ block-BJaaeo/code/script.js
@@ -0,0 +1,13 @@
+let display = document.querySelector('.display');
+
+let root = document.querySelector('.container');
+
+root.addEventListener('click', () => {handleClick(event.target.innerText)});
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+
+function handleClick (event) {
+    if (event in numbers) {
+        
+    }
+}
