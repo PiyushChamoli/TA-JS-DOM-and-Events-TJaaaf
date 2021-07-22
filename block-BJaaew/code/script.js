81dd7fce80ba8c194445af5ed22c4871815574b5
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-07-22 22:22:52 +0530

    update

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -9,41 +9,72 @@ let allPeople = got.houses.reduce((acc,cv) => {
 
 console.log(allPeople);
 
+let activeHouse = '';
 let allTags = got.houses.map((house) => house.name);
 
-allPeople.forEach(person => {
-    let li = document.createElement('li');
-    li.classList.add('flex-30','card');
-
-    let img = document.createElement('img');
-    img.src = person.image;
-    img.alt = person.name;
-
-    let h2 = document.createElement('h2');
-    h2.innerText = person.name;
-
-    let p = document.createElement('p');
-    p.innerText = person.description;
-
-    let btn = document.createElement('a');
-    btn.href = person.wikiLink;
-    btn.innerText = 'Learn More!';
+function createCards(data) {
+    ul.innerHTML = '';
+    data.forEach((person) => {
+        let li = document.createElement('li');
+        li.classList.add('flex-30','card');
     
-    let head = document.createElement('div');
-    head.classList.add('center');
-    head.append(img,h2);
-
-    let footer = document.createElement('div');
-    footer.classList.add('text-center');
-    footer.append(btn);
-
-    li.append(head,p,footer);
-    ul.append(li);
-});
+        let img = document.createElement('img');
+        img.src = person.image;
+        img.alt = person.name;
+    
+        let h2 = document.createElement('h2');
+        h2.innerText = person.name;
+    
+        let p = document.createElement('p');
+        p.innerText = person.description;
+    
+        let btn = document.createElement('a');
+        btn.href = person.wikiLink;
+        btn.innerText = 'Learn More!';
+        
+        let head = document.createElement('div');
+        head.classList.add('center');
+        head.append(img,h2);
+    
+        let footer = document.createElement('div');
+        footer.classList.add('text-center');
+        footer.append(btn);
+    
+        li.append(head,p,footer);
+        ul.append(li);
+    });
+};
 
 function createTagsUI (tags=[]) {
     rootTags.innerHTML = '';
+
     tags.forEach((tag) => {
         let li = document.createElement('li');
+        li.innerText = tag;
+
+        if (activeHouse == tag) {
+            li.classList.add('active');
+        };
+
+        li.addEventListener('click', () => {
+            activeHouse = tag;
+            let peopleOfTheHouse = got.houses.find(house => house.name === tag).people || [];
+            createCards(peopleOfTheHouse);
+            createTagsUI(allTags);
+        });
+
+        rootTags.append(li);
     });
 };
+
+function handleSearch(event) {
+    let text = event.target.value;
+    let filteredPeople = allPeople.filter((p) => p.name.toLowerCase().includes(text.toLowerCase()));
+    createCards(filteredPeople);
+};
+
+search.addEventListener('input', handleSearch);
+
+createTagsUI(allTags);
+
+createCards(allPeople);
